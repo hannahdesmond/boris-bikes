5b257ae0b16bc39f4265239114a05517c48a5b8c
Hannah Desmond <desmond.hannah@googlemail.com>
2021-02-04 12:23:19 +0000

    Update docking_station_spec.rb with an it block that creates a new instance of the Bike class.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,7 +3,8 @@ require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
 
-  it 'gets a bike that is working' do
-     expects(@bike.working?).to eq true
+  it 'releases working bikes' do
+    bike = subject.release_bike
+    expect(bike).to be_working
   end
 end
