036e235a26c6452366c6115b9199f7f9634328eb
Nicolas Bermejo <nicbermejo@gmail.com>
2021-02-04 16:48:20 +0000

    test dock method actually docks and test station holds docked bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,12 +7,23 @@ describe DockingStation do
     bike = subject.release_bike
     expect(bike).to be_working
   end
-
-  it 'docks a bike into docking station' do
+  
+  it 'responds to dock method' do
     expect(subject).to respond_to(:dock).with(1).argument
-    expect(subject.dock(bike)).to eq(bike)
   end
-
-
-
+  
+  it 'docks something' do
+  bike = Bike.new
+  expect(subject.dock(bike)).to eq(bike)
+  end
+  
+  it 'checks a bike is in docking station' do
+  # we want to check that there is a bike in the docking station
+  # after having docked that bike
+  bike = DockingStation.new.release_bike
+  subject.dock(bike)
+  
+  expect(subject.bike).to eq(bike)
+  end
+  
 end
